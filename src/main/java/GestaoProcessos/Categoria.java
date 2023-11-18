2a66bce16aa2aca7105eb766710e514d0011bf33
yodemisj <114534608+yodemisj@users.noreply.github.com>
2023-11-18 18:22:28 -0300

    criação das entidade Categoria e Publicação

---
+++ src/main/java/GestaoProcessos/Categoria.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GestaoProcessos;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author gabriel
+ */
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length=50)
+    private String nome;
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" 
+                + "id=" + id + ", "
+                + "nome=" + nome + '}';
+    }
+}
---
+++ src/main/java/GestaoProcessos/Publicacao.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GestaoProcessos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author gabriel
+ */
+@Entity
+public class Publicacao implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length=50)
+    private String titulo;
+    
+    @Column(length=1000)
+    private String conteudo;
+
+    @OneToOne()
+    private Categoria categoria;
+
+    private LocalDateTime createdAt;
+    
+    private LocalDateTime updatedAt;
+
+    public Publicacao() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Publicacao(String titulo, String conteudo, Categoria categoria) {
+        this();
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+        this.categoria = categoria;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+        setUpdatedAt(LocalDateTime.now());
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+        setUpdatedAt(LocalDateTime.now());
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+        setUpdatedAt(LocalDateTime.now());
+
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updateAt) {
+        this.updatedAt = updateAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Publicacao{" 
+                + "id=" + id + ", "
+                + "titulo=" + titulo + ", "
+                + "conteudo=" + conteudo + ", "
+                + "categoria=" + categoria + ", "
+                + "createdAt="  + createdAt + ", "
+                + "updateAt=" + updatedAt + '}';
+    }
+}
